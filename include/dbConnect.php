5c1247f4006d1f6e6b0679ccbe42dd64dfb9e28a
attajunyah <mikexzibit25@hotmail.com>
2022-01-05 08:15:43 -0800

    updated dbConnect

--- include/dbConnect.php
+++ include/dbConnect.php
@@ -1,16 +1,9 @@
 <?php
+$host="localhost";
+$user="root";
+$pw="12345678";
+$dbn="project";
 
-$host = 'localhost';
-$user = 'root';
-$pw = '919185';
-$dbn = 'project';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-//establish db connection
 $conn = mysqli_connect($host,$user,$pw,$dbn);
-if (mysqli_connect_errno($conn)) {
-	echo 'Something went wrong, failed connect to MYSQL: ' . mysqli_connect_error();
-	exit();
-}
+
 ?>
